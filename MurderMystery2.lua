356ec3b2982a957027416943efd37f3993ff9906
Kam41514 <slmbnekamil@gmail.com>
2026-08-26 00:26:14 +0300

    Update MurderMystery2.lua

--- MurderMystery2.lua
+++ MurderMystery2.lua
@@ -2087,7 +2087,7 @@ State.CoinTeleportEnabled = false
 State.CoinTeleportConnection = nil
 State.CoinTeleportTween = nil
 State.CoinTeleportCharacterConnection = nil
-State.CoinTeleportMurdererAway = false
+State.CoinTeleportMurdererAway = true
 
 
 function funcs.StartCoinTeleport()
@@ -2240,8 +2240,7 @@ function funcs.StartCoinTeleport()
 
 
                 local selectedTarget = nil
-                local selectedPlayerDistance = nil
-                local selectedMurdererDistance = nil
+                local selectedPlayerDistance = math.huge
                 local selectedIndex = nil
 
 
@@ -2273,7 +2272,9 @@ function funcs.StartCoinTeleport()
                                 ).Magnitude
 
 
-                            local murdererDistance = math.huge
+                            local murdererDistance =
+                                math.huge
+
 
                             if murdererRoot then
 
@@ -2288,7 +2289,16 @@ function funcs.StartCoinTeleport()
 
                             if playerDistance <= 5000 then
 
-                                if not selectedTarget then
+                                local murdererIsCloser =
+                                    murdererRoot
+                                    and murdererDistance
+                                    < playerDistance
+
+
+                                if not murdererIsCloser
+                                    and playerDistance
+                                    < selectedPlayerDistance
+                                then
 
                                     selectedTarget =
                                         target
@@ -2296,58 +2306,8 @@ function funcs.StartCoinTeleport()
                                     selectedPlayerDistance =
                                         playerDistance
 
-                                    selectedMurdererDistance =
-                                        murdererDistance
-
                                     selectedIndex =
                                         index
-
-                                else
-
-                                    local shouldSelect = false
-
-                                    if murdererRoot then
-
-                                        if murdererDistance
-                                            > selectedMurdererDistance
-                                        then
-
-                                            shouldSelect = true
-
-                                        elseif murdererDistance
-                                            == selectedMurdererDistance
-                                            and playerDistance
-                                            < selectedPlayerDistance
-                                        then
-
-                                            shouldSelect = true
-
-                                        end
-
-                                    elseif playerDistance
-                                        < selectedPlayerDistance
-                                    then
-
-                                        shouldSelect = true
-
-                                    end
-
-
-                                    if shouldSelect then
-
-                                        selectedTarget =
-                                            target
-
-                                        selectedPlayerDistance =
-                                            playerDistance
-
-                                        selectedMurdererDistance =
-                                            murdererDistance
-
-                                        selectedIndex =
-                                            index
-
-                                    end
 
                                 end
 
@@ -2525,22 +2485,17 @@ Groupboxes.Botting:AddToggle(
     "CoinTeleportToggle",
     {
         Text = "Coin Bot",
-        Default = false
+        Default = true
     }
 ):OnChanged(function(Value)
 
     if Value then
-
         funcs.StartCoinTeleport()
-
     else
-
         funcs.StopCoinTeleport()
-
     end
 
 end)
-
 
 
 
