7a0b7e987cd31a3aada0750af9b8b00433b67389
Kam41514 <slmbnekamil@gmail.com>
2026-08-28 06:12:26 +0300

    Update Updates.lua

--- Updates/Updates.lua
+++ Updates/Updates.lua
@@ -7,7 +7,7 @@ local Modules = {
 
     {
         Name = "ESP",
-        Description = "Better ESP Settings to optimize.",
+        Description = "Showing closest player locations based on Chakra Points.",
         Status = "Added"
     },
 
@@ -15,7 +15,27 @@ local Modules = {
         Name = "Aimbot",
         Description = "Fixed Circle and FPS Optimization.",
         Status = "Fixed"
-    }
+    },
+    {
+        Name = "AutoClicker",
+        Description = "It will be added soon.",
+        Status = "Removed"
+    },
+    {
+        Name = "Anti AFK",
+        Description = "Simple Anti AFK Script.",
+        Status = "Added"
+    },
+    {
+        Name = "Auto Block",
+        Description = "It's still not improved and buggy.",
+        Status = "Beta"
+    },
+    {
+        Name = "Exploiter Detector",
+        Description = "Buggy but still works fine.",
+        Status = "Beta"
+    },
 }
 
 return Modules
