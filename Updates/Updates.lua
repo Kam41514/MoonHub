e59fe32a56574d10ceb9f737c392a39e4421844e
Kam41514 <slmbnekamil@gmail.com>
2026-08-28 05:32:23 +0300

    Create Updates.lua

---
+++ Updates/Updates.lua
@@ -0,0 +1,21 @@
+local Modules = {
+    {
+        Name = "NoClip",
+        Description = "Added transparency to close walls for better sight.",
+        Status = "Updated"
+    },
+
+    {
+        Name = "ESP",
+        Description = "Better ESP Settings to optimize.",
+        Status = "Added"
+    },
+
+    {
+        Name = "Aimbot",
+        Description = "Fixed Circle and FPS Optimization.",
+        Status = "Fixed"
+    }
+}
+
+return Modules
