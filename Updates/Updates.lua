a9641f6f0312a6784c3adb4de0944aa5d1c1f744
Kam41514 <slmbnekamil@gmail.com>
2026-08-28 06:21:33 +0300

    Update Updates.lua

--- Updates/Updates.lua
+++ Updates/Updates.lua
@@ -1,4 +1,6 @@
 local Modules = {
+    LastUpdate = "August 28, 2026",
+
     {
         Name = "NoClip",
         Description = "Added transparency to close walls for better sight.",
@@ -16,26 +18,31 @@ local Modules = {
         Description = "Fixed Circle and FPS Optimization.",
         Status = "Fixed"
     },
+
     {
         Name = "AutoClicker",
         Description = "It will be added soon.",
         Status = "Removed"
     },
+
     {
         Name = "Anti AFK",
         Description = "Simple Anti AFK Script.",
         Status = "Added"
     },
+
     {
         Name = "Auto Block",
         Description = "It's still not improved and buggy.",
         Status = "Beta"
     },
+
     {
         Name = "Exploiter Detector",
         Description = "Buggy but still works fine.",
         Status = "Beta"
     },
+
     {
         Name = "No Stun",
         Description = "Changes the Value of Stunned but doesn't remove speed stun.",
